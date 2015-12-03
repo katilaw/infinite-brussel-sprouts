e52938dcafb990f801fa7fe2b61e103c3d3b9824
Jonathan Devers <jonathan.devers@gmail.com>
2015-12-03 15:09:20 -0500

    add infinite scroll

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,22 +1,50 @@
 var pageNum = 2;
+$("a").hide();
 
-$( ".more-sprouts" ).click(function(event) {
-  event.preventDefault();
+// $( ".more-sprouts" ).click(function(event) {
+//   event.preventDefault();
+//
+//   // pageNum += 1;
+//
+//   var response = $.ajax({
+//     method: "GET",
+//     dataType: "JSON",
+//     url: "/tweets.json?page=" + pageNum++,
+//     // url: "/tweets.json",
+//     success: function( json ) {
+//       json.forEach(function( tweet ){
+//         $(".tweets").append("<li class='tweet'><div class='body'>" + tweet.text +"<div class='user'>" + tweet.username + "</div></li>");
+//         $(".more-sprouts").attr("href", ("/tweets?page=" + pageNum))
+//       });
+//     }
+//
+//   });
+//
+// });
 
-  // pageNum += 1;
+var getMoreTweets = function() {
+  if($(window).scrollTop() + $(window).height() == $(document).height()) {
 
-  var response = $.ajax({
-    method: "GET",
-    dataType: "JSON",
-    url: "/tweets.json?page=" + pageNum++,
-    // url: "/tweets.json",
-    success: function( json ) {
-      json.forEach(function( tweet ){
-        $(".tweets").append("<li class='tweet'><div class='body'>" + tweet.text +"<div class='user'>" + tweet.username + "</div></li>");
-        $(".more-sprouts").attr("href", ("/tweets?page=" + pageNum))
-      });
-    }
+    var response = $.ajax({
+      method: "GET",
+      dataType: "JSON",
+      url: "/tweets.json?page=" + pageNum++,
+      // url: "/tweets.json",
+      success: function( json ) {
+        json.forEach(function( tweet ){
+          $(".tweets").append("<li class='tweet'><div class='body'>" + tweet.text +"<div class='user'>" + tweet.username + "</div></li>");
+          $(".more-sprouts").attr("href", ("/tweets?page=" + pageNum))
+        });
+      }
 
-  });
+    });
+  }
+};
 
-});
+$(window).scroll(getMoreTweets);
+
+
+// This code exists for situations when monitor size is large
+// enough to accomodate the first round of tweets, resulting in no scroll bar.
+
+getMoreTweets();
